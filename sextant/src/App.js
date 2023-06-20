c4401ceb7f95fe2af5bbcc28e14452315e8a150c
Rowland Oka <rowlandoka@gmail.com>
2023-06-20 22:43:41 +0100

    Added latency display component

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Banner from "./Banner";
 import Exhibit from "./Exhibit";
 import IPAddressDisplay from "./IPAddressDisplay";
+import NetLatency from "./NetLatency";
 
 class App extends Component {
   render() {
@@ -14,6 +15,9 @@ class App extends Component {
         </Exhibit>
         <Exhibit name="Public IPv6 Address">
           <IPAddressDisplay url="https://api64.ipify.org?format=json" />
+        </Exhibit>
+        <Exhibit name="Network Packet Latency">
+          <NetLatency />
         </Exhibit>
       </div>
     );
--- sextant/src/NetLatency.js
+++ sextant/src/NetLatency.js
@@ -1,5 +1,25 @@
 import React, { Component } from "react";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+const client = new W3CWebSocket("ws://localhost:55455");
 
-class NetLatency extends Component {}
+class NetLatency extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latency: null,
+    };
+  }
+
+  componentDidMount() {
+    client.onmessage = (message) => {
+      this.setState({
+        latency: new Date().getTime() - message.data,
+      });
+    };
+  }
+  render() {
+    return <span className="NetLatency">{this.state.latency}</span>;
+  }
+}
 
 export default NetLatency;
